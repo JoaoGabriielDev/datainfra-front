52c539d6424c0e7ba1c8563e60f5ab957b9fd903
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-08-24 12:27:29 -0300

    Inputs

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginform!: FormGroup;
+
+  constructor(){
+    this.loginform = new FormGroup({
+      email: new FormGroup('', [Validators.required, Validators.email]),
+      password: new FormGroup('', [Validators.required, Validators.minLength(8)])
+    })
+  }
 
 }
